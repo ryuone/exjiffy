f0b158ba466dd1e4692f0ceb964106111a42d6d6
Ryuichi Maeno <ryuone@gmail.com>
2013-09-05 09:42:29 +0900

    Add defdelegate for jiffy.

--- lib/exjiffy.ex
+++ lib/exjiffy.ex
@@ -1,2 +1,3 @@
 defmodule Exjiffy do
+  defdelegate [decode(data), encode(data), encode(data, options)], to: :jiffy
 end
--- mix.exs
+++ mix.exs
@@ -16,6 +16,8 @@ defmodule Exjiffy.Mixfile do
   # Returns the list of dependencies in the format:
   # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    []
+    [
+      { :jiffy, github: "davisp/jiffy" }
+    ]
   end
 end
